cfc2486c2dedd835254d325dadb3894aa40aaaef
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-17 13:07:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ def word_reverse(word)
 end
 def reverse_each_word(str)
   str_arr=str.split(" ")
-  arr=str_arr.collect  do |word|
+  arr=str_arr.collect do |word|
     word_reverse(word)
   end
   return arr.join(" ")
