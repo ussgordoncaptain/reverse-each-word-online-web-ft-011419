795b0a93f8502b55fcb1476542c95d9d84d132b8
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-17 13:03:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,10 @@ def word_reverse(word)
   final = ""
   ii = word.length 
   while(ii >= 0 )
+    final += word(ii)
+  end
+  return final 
 end
-  
+def reverse_each_word(str)
+  str_arr=str.split(" ")
+  str_arr.collect  
